0625fbee52047b6d84e2988b1dd5d6f2ecd98d69
James Gower <66065325+James-G72@users.noreply.github.com>
2021-02-13 23:44:47 +0000

    Adding the ability to pull the historical price table from the website

--- CryptoAttributes/store.py
+++ CryptoAttributes/store.py
@@ -1,28 +1,56 @@
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
 class CryptoStorage():
     """
     CryptoStorage handles the .csv files and all data
     """
-    def __init__(self, first_coin, webiste):
+    def __init__(self, first_coin, website):
         """
         Initialises the object with a first
-        :param first_coin:
-        :param webiste:
+        :param first_coin: The storage object is initialised with the current price of a single coin
+        :param webiste: The home page of the website that the object is to use to fetch data
         """
+        self.data_origin = website
         self.latest_prices = {}
-        self.Current_price(first_coin, self.latest_prices)
+        self.Current_price(first_coin)
 
-        self.historic_data = pd.dataframe(columns=[first_coin])
+        # self.historic_data = pd.dataframe(columns=[first_coin])
 
-    def Current_price(self):
+    def Current_price(self, *coins):
+        """
+        Gets the current price of the requested coin. Uses self.data_origin to know where to look
+        :param coins: Could be any number of coins in a list
+        :return: time_stamp - The exact time the data was true, prices - Dictionary of coins and prices
+        """
 
+        def Single_value_scrape(url):
+            r = requests.get(url)
+            html = r.content
+            page = BeautifulSoup(html, "html.parser")
+            return page.find(id="initial_price")["value"]
 
-    def Full_data_loader(self, coins, website):
+        latest = self.latest_prices
+        for coin in coins:
+            url = self.data_origin+'/coins/'+coin+'/gbp'
+            price = Single_value_scrape(url)
+            latest[coin] = price
+        self.latest_prices = latest
+
+    def Full_price_loader(self, coin):
         """
         Used to load the full history of specified coins from the website
         :param coins: A dictionary or array of coins to be processed
-        :param website: The front of a website. Coingecko is the only one which curretly works
+        :param website: The front of a website. Coingecko is the only one which currently works
         :return: None
         """
+        url = self.data_origin+'/coins/'+coin+'/historical_data/gbp#panel'
+        r = requests.get(url)
+        html = r.content
+        page = BeautifulSoup(html,"html.parser")
+        table_string = page.table.tbody.text
+        stuff = list(filter(None, table_string.splitlines()))
+
+        print(stuff)
 
--- host.py
+++ host.py
@@ -1,67 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-import json
-import time
+import CryptoAttributes as CA
 
+coin_gecko = 'https://www.coingecko.com/en'
 
-def get_price(*coins):
-    def scrape(url):
-        r = requests.get(url)
-        html = r.content
-        page = BeautifulSoup(html, "html.parser")
-        print(page)
-        return page.find(id="initial_price")["value"]
+storage = CA.CryStor("bitcoin", coin_gecko)
 
-    output = {}
-    for coin in coins:
-        url = 'https://www.coingecko.com/en/coins/'+coin+'/gbp'
-        price = scrape(url)
-        output[coin] = price
+storage.Full_price_loader("bitcoin")
 
-    return output
-
-results = get_price(
-    'bitcoin',
-    'ethereum',
-    'ripple',
-    'golem')
-
-for coin,price in results.items():
+for coin, price in storage.latest_prices.items():
     print(" - ",coin + ":", price)
-
-
-
-
-# cmc = requests.get("https://www.coingecko.com/en")
-# soup = BeautifulSoup(cmc.content, "html.parser")
-# data = soup.find("script", id="__NEXT_DATA__", type="application/json")
-# coins = {}
-# coin_data = json.loads(data.contents[0])
-# listings = coin_data["props"]["initialState"]["cryptocurrency"]["listingLatest"]["data"]
-# for i in listings:
-#     coins[str(i["id"])] = i["slug"]
-#
-# market_cap = []
-# volume = []
-# timestamp = []
-# name = []
-# symbol = []
-# slug = []
-#
-# for i in coins:
-#     page = requests.get(f"https://www.coingecko.com/en/coins/bitcoin/historical_data/gbp")
-#     soup = BeautifulSoup(page.content, "html.parser")
-#     data = soup.find("script", id="__NEXT_DATA__", type="application/json")
-#
-# for i in coins:
-#     historical_data = json.loads(data.contents[0])
-#     quotes = historical_data["props"]["initialState"]["cryptocurrency"]["ohlcvHistorical"][i]["quotes"]
-#     info = historical_data['props']['initialState']['cryptocurrency']['ohlcvHistorical'][i]
-#     for j in quotes:
-#         market_cap.append(j["quote"]["USD"]["market_cap"])
-#         volume.append(j["quote"]["USD"]["volume"])
-#         timestamp.append(j["quote"]["USD"]["timestamp"])
-#         name.append(info["name"])
-#         symbol.append(info["symbol"])
-#         slug.append(coins[i])
